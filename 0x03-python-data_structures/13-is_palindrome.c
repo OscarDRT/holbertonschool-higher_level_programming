3278845562d1c1cebe1e2c96d5372aab975ecfc4
oscar <oscarnetworkingpro@gmail.com>
2019-12-17 20:47:11 -0500

    Efficiency success 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ *list_len - Returns the number of elements in a linked list_t list
+ * @h: contains the header of the list
+ * Return: Number of nodes
+ */
+size_t list_len(listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+/**
  * is_palindrome - function that checks if a singly linked list is a palindrome
  * @head: head of th list
  * Return: 1 is palindrome or 0 is not palindrome
@@ -8,16 +24,11 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *ti = *head, *ts = *head;
-	int i = 1, j = 1, len = 0;
+	size_t i = 1, j = 1, len = 0;
 
 	if (head == NULL || *head == NULL)
 		return (1);
-	while (ti)
-	{
-		len++;
-		ti = ti->next;
-	}
-	ti = *head;
+	len = list_len(ti);
 	while (i != len)
 	{
 		for (j = 1; j < len; j++)
