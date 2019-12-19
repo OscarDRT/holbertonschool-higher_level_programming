af0e30ec15e2004fa6ff51cb0ce7b811476ce6cb
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2019-12-19 18:02:54 -0500

    roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     ant = roman[roman_string[0]]
-    if type(roman_string) is not str or roman_string == '':
+    if not roman_string:
         return 0
     else:
         for i in roman_string:
