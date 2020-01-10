3d86c4ec47c6b48d3c6c26054f8d01b4ec32b50a
oscar <oscarnetworkingpro@gmail.com>
2020-01-10 00:01:23 -0500

    emer

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,11 +10,12 @@ class Square:
 
     @size.setter
     def size(self, value):
-        self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @property
     def position(self):
@@ -22,10 +23,12 @@ class Square:
 
     @position.setter
     def position(self, value):
-        self.__position = value
+        msn = "position must be a tuple of 2 positive integers"
         if (type(value) != tuple or value[0] != int or value[1] != int
-        or len(value) != 2 or value[0] < 0 or value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
+            or len(value) != 2 or value[0] < 0 or value[1] < 0):
+                raise TypeError(msn)
+        else:
+              self.__position = value
 
     def area(self):
         return (self.__size ** 2)
@@ -33,9 +36,9 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in range(0, self.__position[1]):
-            print()
-        for i in range(0, self.__size):
-            print(" " * self.__position[0], end='')
-            print("#" * self.__size, end='')
-            print()
+        else:
+            for i in range(0, self.__position[1]):
+                print()
+            for i in range(0, self.__size):
+                print(" " * self.__position[0], end='')
+                print("#" * self.__size)
