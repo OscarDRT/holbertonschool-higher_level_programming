6b0173b1d07f526af88ed805cd650c53e5820809
oscar <oscarnetworkingpro@gmail.com>
2020-01-12 15:07:52 -0500

    ad

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Function that divides all elements of a matrix.
+Returns a new matrix
+matrix must be a list of lists of integers or floats,
+otherwise raise a TypeError exception or ZeroDivisionError exception
+"""
+
+
 def matrix_divided(matrix, div):
+    """
+        matrix must be a list of lists of integers or floats
+    """
     msn1 = "matrix must be a matrix (list of lists) of integers/floats"
     msn2 = "Each row of the matrix must have the same size"
     msn3 = "div must be a number"
@@ -18,9 +28,6 @@ def matrix_divided(matrix, div):
                     raise TypeError(msn1)
     if type(div) != int and type(div) != float:
         raise TypeError(msn3)
-    elif div == 0:
+    if div == 0:
         raise ZeroDivisionError(msn4)
-    else:
-        return list(map(lambda i: list(map(lambda j: round(j / div, 2), i)), matrix))
-
-
+    return list(map(lambda i: list(map(lambda j: round(j/div, 2), i)), matrix))
