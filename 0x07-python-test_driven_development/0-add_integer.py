485c11d3fd9b9fec095dc8f5fd868d6a194ffb6f
oscar <oscarnetworkingpro@gmail.com>
2020-01-12 14:09:38 -0500

    test 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Function that adds 2 integers.
+Return an integer: The addition of a and b
+a and b must be integers or floats, otherwise raise a TypeError
+exception with the messange a must be an integer or b must be an integer
+"""
+
+
 def add_integer(a, b=98):
+    """
+        a and b must be integers or floats
+    """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [1, 2, 3]
-]
-print(matrix_divided([1, 2, 3], 3))
-print(matrix)
+
+print(matrix_divided([[1, 1.22],[2, 5.9],[0, 10]], None))
