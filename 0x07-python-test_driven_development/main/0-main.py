714cc2abdbb80ffe0b2abf0c5a8ef7fedaeb0055
oscar <oscarnetworkingpro@gmail.com>
2020-01-13 23:20:46 -0500

    add case 0

--- 0x07-python-test_driven_development/main/0-main.py
+++ 0x07-python-test_driven_development/main/0-main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer())
-print(add_integer(100, -2))
+print(add_integer(100, True))
 print(add_integer(2))
 print(add_integer(100.3, -2))
 try:
