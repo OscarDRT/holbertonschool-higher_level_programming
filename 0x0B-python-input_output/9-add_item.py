08035db579c3ba86d47cf7fe655d0cfd5e580387
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-01-21 17:33:10 -0500

    REgarcha

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-import json, sys
+import json
+import sys
 
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
-new_list = []
-filename = "add_item.json"
-for i in range(1, len(sys.argv)):
-    new_list.append(sys.argv[i])
-save_to_json_file(new_list, filename)
-load_from_json_file(filename)
+try:
+    new_list = load_from_json_file("add_item.json")
+except:
+    new_list = []
+finally:
+    for i in range(1, len(sys.argv)):
+        new_list.append(sys.argv[i])
+    save_to_json_file(new_list, "add_item.json")
