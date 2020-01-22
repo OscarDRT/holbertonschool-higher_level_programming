936fd97fea2af28db66ae5793450646187f56965
oscar <oscarnetworkingpro@gmail.com>
2020-01-21 22:29:39 -0500

    asdfg

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -2,11 +2,16 @@
 def pascal_triangle(n):
     if n <= 0:
         my_list = [[]]
-    if n == 1:
-        my_list = [[1]]
     else:
-        my_list = [[1], [1, 1]]
-    for i in range(my_list):
-        for j in range(my_list[i]):
-            print my_list[i][j]
-    return my_list
+        final = []
+        for index in range(1, n + 1):
+            new_list = []
+            for x in range(index):
+                new_list.append(1)
+            final.append(new_list)
+
+        for i in range(len(final)):
+            for j in range(len(final[i])):
+                if j != 0 and j != len(final[i - 1]):
+                    final[i][j] = final[i - 1][j] + final[i - 1][j - 1]
+        return final
