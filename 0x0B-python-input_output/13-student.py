c3c2620bb5f7e34e744ff96d3161ada2fa67fdb2
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-01-21 19:23:41 -0500

    setattr

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        my_dict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
