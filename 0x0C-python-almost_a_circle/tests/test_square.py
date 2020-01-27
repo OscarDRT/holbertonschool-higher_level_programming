30ecf3a58201ce6c5ad6c537a650ddf1569f6ae8
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-01-27 17:51:06 -0500

    asd

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""tess dsf"""
-
-
 import unittest
 import pep8
 from models.base import Base
@@ -10,7 +7,6 @@ from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
-    """test"""
     def test_pep8_conformance_square(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
@@ -18,71 +14,68 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(result.total_errors, 0, "Found code style errors \
             (and warnings).")
 
+    """Task 11"""
     def test_getter(self):
-        """test"""
         r1 = Square(5)
         self.assertEqual(r1.size, 5)
 
     def test_setter(self):
-        """test"""
         r1 = Square(5)
         r1.size = 8
-        self.assertEqual(r1.size, 8    def test_string(self):
+        self.assertEqual(r1.size, 8)
+
+    def test_string(self):
         r1 = Square(3)
         with self.assertRaises(TypeError):
             r1.size = "Hi"
 
     def test_negative(self):
-        """test"""
         r1 = Square(6)
         with self.assertRaises(ValueError):
             r1.size = -5
 
     def test_zero(self):
-        """test"""
         r1 = Square(6)
         with self.assertRaises(ValueError):
             r1.size = 0
 
-    def test_zero(self):
-        """test"""
+    def test_zero_1(self):
         r1 = Square(6)
         with self.assertRaises(TypeError):
             r1.size = 1.5
 
     def test_tupla(self):
-        """test"""
         r1 = Square(7)
         with self.assertRaises(TypeError):
             r1.size = (2, 8)
 
     def test_empty(self):
-        """test"""
         r1 = Square(7)
         with self.assertRaises(TypeError):
             r1.size = ""
 
     def test_none(self):
-        """test"""
         r1 = Square(5)
         with self.assertRaises(TypeError):
             r1.size = None
 
     def test_list(self):
-        """test"""
         r1 = Square(4)
         with self.assertRaises(TypeError):
             r1.size = [4, 7]
 
     def test_dict(self):
-        """test"""
         r1 = Square(5)
         with self.assertRaises(TypeError):
             r1.size = {"hi": 5, "world": 8}
 
     def test_width(self):
-        """test"""
         r1 = Square(5)
         r1.size = 6
         self.assertEqual(r1.width, 6)
         self.assertEqual(r1.height, 6)
+
+
+    """end task 11"""
+if __name__ == "_main_":
+    unittest.main()
