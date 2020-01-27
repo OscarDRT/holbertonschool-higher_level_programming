bd374a6f17e0cf60e84b208797c673340955b547
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-01-27 14:30:53 -0500

    push

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -63,7 +63,10 @@ class Base:
         """
         Create
         """
-        dummy = cls(1, 1, 1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
