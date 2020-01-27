04f1412e3c3b6893b9364f2fea62ef4f02879abd
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-01-27 14:02:31 -0500

    square com

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
+"""
+class Square that inherits from Rectangle
+"""
+
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """class Square"""
     def __init__(self, size, x=0, y=0, id=None):
+        """comment"""
         super().__init__(size, size, x, y, id)
         self.width = size
         self.height = size
 
     @property
     def size(self):
+        """comment"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """comment"""
         self.width = value
         self.height = value
 
     def __str__(self):
+        """comment"""
         m = "[Square] ({}) {}/{} - {}"
         return m.format(self.id, self.x, self.y, self.width)
 
     def update(self, *args, **kwargs):
+        """comment"""
         if len(args) != 0:
             name = ["id", "size", "x", "y"]
             i = 0
@@ -33,6 +44,7 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """comment"""
         name = ["id", "size", "x", "y"]
         value = [self.id, self.width, self.x, self.y]
         x = {}
