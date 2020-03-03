58c31518cb90fa821df5f53dfe6d29d5fc8184d8
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-03-03 16:03:58 -0500

    echo 'SELECT * FROM states;' | mysql -hlocalhost -uroot -p hbtn_0d_usa

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 -- (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id UNIQUE AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE AUTO_INCREMENT NOT NULL,
 name VARCHAR(256) NOT NULL,
 CONSTRAINT states PRIMARY KEY (id));
