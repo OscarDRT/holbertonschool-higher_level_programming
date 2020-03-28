193e4edabb4429b5957fe2d0fb69ab01572de371
oscar <oscarnetworkingpro@gmail.com>
2020-03-28 12:17:45 -0500

    script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+"""script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 import sys
 import MySQLdb
@@ -13,7 +14,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                   WHERE states.name LIKE 'N%'
+                   WHERE states.name LIKE '%N%'
                    ORDER BY states.id ASC""")
 
     query_rows = cur.fetchall()
