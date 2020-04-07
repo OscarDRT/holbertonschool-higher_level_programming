26912686237b34cf67507415e3bf43409b440976
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-06 21:26:26 -0500

    Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ash script that takes in a URL,
+# sends a request to that URL,
+# and displays the size of the body of the response
+
+curl --HEAD $1 | grep -i Content-Length | awk '{print $2}'
