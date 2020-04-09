c06b765a9e5b537e825146c0c17d2d1ef1d13c92
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-09 13:52:57 -0500

    Change

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,10 @@
 
 
 def find_peak(list_of_integers):
-    try:
-        return max(list_of_integers)
-    except:
-        return None
+    if list_of_integers:
+        max = list_of_integers[0]
+        for num in list_of_integers:
+            if num >= max:
+                max = num
+        return max
+    return None
