7869ec32e9154757f59c771a909ff66adbd8f6d2
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-09 12:30:20 -0500

    Update changes

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL
-# and displays all HTTP methods the server will accept
-
+# Bash script that takes in a URL and displays all HTTP methods the server will accept
 curl -I "$1" | grep "Allow" | awk -F ':' '{print $2}'
