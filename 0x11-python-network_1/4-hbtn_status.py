afec8b2bfce836356f38d91e8cfbe3d13eeec53a
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-15 13:00:10 -0500

    Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     pag = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("    - type: {}".format(type(pag.text)))
-    print("    - content: {}".format(pag.text))
+    print("\t- type: {}".format(type(pag.text)))
+    print("\t- content: {}".format(pag.text))
