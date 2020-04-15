5f6d25293e060ccee64194a13c2f0ecd3714b6c0
OscarDRT <oscarnetworkingpro@gmail.com>
2020-04-15 12:58:19 -0500

    script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,13 +4,13 @@ import urllib.request
 from sys import argv
 
 
-url = argv[1]
-req = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(req) as response:
-        if response.status == 200:
-            print(response.read().decode('utf-8'))
-        else:
-            print("Error code: {}".format(response.status))
-except urllib.error.URLError as error:
-    print("Error code: {}".format(error.code))
+if __name__ == '__main__':
+    try:
+        url = argv[1]
+        with urllib.request.urlopen(url) as pag:
+            if pag.status == 200:
+                print(pag.read().decode('utf-8'))
+            else:
+                print("Error code: {}".format(pag.status))
+    except Exception as e:
+        print("Error code: {}".format(e.code))
