bca5301e95a6c431db7abf45ab1ef64c83eba76f
OscarDRT <oscarnetworkingpro@gmail.com>
2020-05-05 18:35:08 +0000

    add script factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2], 10);
+const result = factorial(num);
+console.log(result);
