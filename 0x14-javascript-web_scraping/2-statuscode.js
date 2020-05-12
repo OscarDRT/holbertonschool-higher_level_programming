120316d19ba5a60ad57a9e34cca42a8656028135
OscarDRT <oscarnetworkingpro@gmail.com>
2020-05-12 12:19:50 -0500

    statuscode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
   console.log('Code:', response.statusCode); // Print the response status code if a response was received
 });
