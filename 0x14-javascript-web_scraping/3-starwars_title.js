eaca93c3c6544edb1409c56c46fa170989b9cbe2
OscarDRT <oscarnetworkingpro@gmail.com>
2020-05-12 14:43:10 -0500

    title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,5 +6,5 @@ const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id + '/';
 
 request.get(url, function (error, response, body) {
-  if (response.statusCode === 404) { console.log(body); } else { console.log(JSON.parse(body).title); }
+  if (error) { console.log(error); } else { console.log(JSON.parse(body).title); }
 });
