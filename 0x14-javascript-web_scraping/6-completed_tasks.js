6a574475d78c194a207c3bae61d326a934401b73
OscarDRT <oscarnetworkingpro@gmail.com>
2020-05-12 14:22:41 -0500

    Add

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,11 +4,13 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (err, response, body) => {
+  const obj = {};
   if (err) { console.log(err); } else {
     const todos = JSON.parse(body);
-    const obj = {};
     todos.forEach(element => {
-      obj[element.userId] = 0;
+      if (!(obj[element.userId])) {
+        obj[element.userId] = 0;
+      }
     });
     todos.forEach(element => {
       if (element.completed === true) {
